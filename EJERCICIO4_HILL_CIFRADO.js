1ab12d10eca9a1a4c354c6c300d72b1fa205fd28
Miguel Crispin <crispoo@outlook.com>
2023-09-29 11:15:57 -0600

    new file:   EJERCICIO4_HILL_CIFRADO.js 	modified:   EJERCICIO4_HILL_DESCIFRADO.js

---
+++ EJERCICIO4_HILL_CIFRADO.js
@@ -0,0 +1,118 @@
+console.clear()
+
+//ALUMNO: MIGUEL ANGEL CRISPIN CABRERA
+// Método de HILL
+
+let mensajeACifrar = "CODIGO"
+console.log("Mensaje: "+mensajeACifrar)
+console.log("")
+
+// const ALFABETO_ESPAÑOL = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const ALFABETO_ESPAÑOL = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+let matrizLlave = [
+    [5, 17, 20],
+    [9, 23, 3],
+    [2, 11, 13]
+]
+
+console.log("Matriz Llave")
+console.table(matrizLlave)
+
+function matrizarMensaje(mensajeAMatrizar) {
+    //SE CONVIERTE EL MENSAJE ORIGINAL A UN ARREGLO DE CARÁTERES, SIN ESPACIOS NI COMILLAS
+    let matriz = (Array.from(mensajeAMatrizar.toUpperCase())).filter(element => ((element != " ") && (element != ",")))
+    // imprimirMatriz(matrizGeneral,"MatrizMatriz")
+    return matriz
+}
+
+
+let arregloUnidimensional = matrizarMensaje(mensajeACifrar)
+console.log("")
+console.log("Matriz Unidimensional")
+console.log(arregloUnidimensional)
+// console.table(arregloUnidimensional)
+
+function obtenerValorAlfabeto(arregloLetras, alfabeto) {
+    let indicesAlfabeto = []
+    arregloLetras.forEach(element => {
+        indicesAlfabeto.push(alfabeto.indexOf(element))
+    })
+    return indicesAlfabeto
+}
+
+let valoresAlfabeto = obtenerValorAlfabeto(arregloUnidimensional, ALFABETO_ESPAÑOL)
+console.log("")
+console.log("Reemplazo de Indices")
+console.log(valoresAlfabeto)
+// console.table(valoresAlfabeto)
+
+
+
+function separarMatriz(lista, limiteSubElementos) {
+    //Separar matriz unidimensional, en bidimensional con parámetro de limite en cada subelemento
+    var matrix = [], i, k;
+    for (i = 0, k = -1; i < lista.length; i++) {
+        if (i % limiteSubElementos === 0) {
+            k++;
+            matrix[k] = [];
+        }
+        matrix[k].push(lista[i]);
+    }
+    return matrix;
+}
+
+let matrizSeparada = separarMatriz(valoresAlfabeto, 3);
+console.log("")
+console.log("Matriz segmentada")
+console.table(matrizSeparada)
+
+
+function multiplicar(llave, matrizSegmentada) {
+    var result = [];
+    let sumar1=0;
+    let sumar2=0;
+    let sumar3=0
+    matrizSegmentada.forEach(element  => {
+        sumar1 = element[0]*llave[0][0] + element[1]*llave[0][1] + element[2]*llave[0][2]
+        sumar2 = element[0]*llave[1][0] + element[1]*llave[1][1] + element[2]*llave[1][2]
+        sumar3 = element[0]*llave[2][0] + element[1]*llave[2][1] + element[2]*llave[2][2]
+        sumar1 > 0 ? result.push(sumar1) : ""
+        sumar2 > 0 ? result.push(sumar2) : ""
+        sumar3 > 0 ? result.push(sumar3) : ""
+
+    })
+    return result;
+}
+
+
+let matrizMultiplicada = multiplicar(matrizLlave,matrizSeparada)
+console.log("")
+console.log("Multiplicacion MatrizLlave*MatrizSegmentada")
+console.table(matrizMultiplicada)
+
+function reemplazar (arregloCalculado){
+    let arrayCifrado = []
+    arregloCalculado.forEach(element => {
+        let mod = element % ALFABETO_ESPAÑOL.length
+        arrayCifrado.push(ALFABETO_ESPAÑOL[mod])
+    })
+    arrayCifrado = arrayCifrado.join("")
+    return arrayCifrado
+}
+
+
+let stringCifrado = reemplazar(matrizMultiplicada)
+console.log("")
+console.log("Cifrado")
+console.log(stringCifrado)
+
+
+
+
+
+
+
+
+
+
--- EJERCICIO4_HILL_DESCIFRADO.js
+++ EJERCICIO4_HILL_DESCIFRADO.js
@@ -1,50 +1,111 @@
-console.clear()
 
-//ALUMNO: MIGUEL ANGEL CRISPIN CABRERA
-// Método de HILL
-const ALFABETO_ORIGINAL = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"] 
-let matrizLlave = new Array(2);
-matrizLlave[0] = new Array(2);
-matrizLlave[1] = new Array(2);
-matrizLlave[0][0] = 1
-matrizLlave[0][1] = 2
-matrizLlave[1][0] = 3
-matrizLlave[1][1] = 4
-
-console.log(matrizLlave)
-
-function desplazar( q , alfabeto_ordenar) {
-    const reordenar = Array.from(alfabeto_ordenar)
-    for (let i = 0; i < q; i++) {
-        reordenar.push(reordenar[0]);
-        reordenar.shift();
+function print(M, msg) {
+    console.log("======" + msg + "=========")
+    for(var k=0; k<M.length; ++k) {
+      console.log(M[k]);
     }
-    return reordenar;
-}
+    console.log("==========================")
+  }
+  
+  function diagonalize(M) {
+    var m = M.length;
+    var n = M[0].length;
+    for(var k=0; k<Math.min(m,n); ++k) {
+      // Find the k-th pivot
+      i_max = findPivot(M, k);
+      if (A[i_max, k] == 0)
+        throw "matrix is singular";
+      swap_rows(M, k, i_max);
+      // Do for all rows below pivot
+      for(var i=k+1; i<m; ++i) {
+        // Do for all remaining elements in current row:
+        var c = A[i][k] / A[k][k];
+        for(var j=k+1; j<n; ++j) {
+          A[i][j] = A[i][j] - A[k][j] * c;
+        }
+        // Fill lower triangular matrix with zeros
+        A[i][k] = 0;
+      }
+    }
+  }
+  
+  function findPivot(M, k) {
+    var i_max = k;
+    for(var i=k+1; i<M.length; ++i) {
+      if (Math.abs(M[i][k]) > Math.abs(M[i_max][k])) {
+        i_max = i;
+      }
+    }
+    return i_max;
+  }
+  
+  function swap_rows(M, i_max, k) {
+    if (i_max != k) {
+      var temp = A[i_max];
+      A[i_max] = A[k];
+      A[k] = temp;
+    }
+  }
+  
+  function makeM(A, b) {
+    for(var i=0; i<A.length; ++i) {
+      A[i].push(b[i]);
+    }
+  }
+  
+  function substitute(M) {
+    var m = M.length;
+    for(var i=m-1; i>=0; --i) {
+      var x = M[i][m] / M[i][i];
+      for(var j=i-1; j>=0; --j) {
+        M[j][m] -= x * M[j][i];
+        M[j][i] = 0;
+      }
+      M[i][m] = x;
+      M[i][i] = 1;
+    }
+  }
+  
+  function extractX(M) {
+    var x = [];
+    var m = A.length;
+    var n = A[0].length;
+    for(var i=0; i<m; ++i){
+      x.push(A[i][n-1]);
+    }
+    return x;
+  }
+  
+  function solve(A, b) {
+    //print(A, "A");
+    makeM(A,b);
+    //print(A, "M");
+    diagonalize(A);
+    //print(A, "diag");
+    substitute(A);
+    //print(A, "subst");
+    var x = extractX(A);
+    //print(x, "x");
+    return x;
+  }
+  
+  // sample from: http://mathworld.wolfram.com/GaussianElimination.html
+  
+  A = [
+    [9,3,4],
+    [4,3,4],
+    [1,1,1]
+  ]
+  
+  b = [7,8,3]
+  
+  print(A, " A ");
+  print(b, " b ");
+  
+  var x = solve(A, b);
+  
+  print(x, " x ");
 
-let ALFABETO_DESPLAZADO = desplazar(3,ALFABETO_ORIGINAL)
 
-function cifrar(mensaje,inner_ALFABETO,inner_ALFABETO_DESPLAZADO){
-    mensaje = mensaje.toUpperCase()
-    let mensajeArreglo= Array.from(mensaje)
-    let arregloFinal = []
-    // console.log(mensajeArreglo)
-    //console.log(mensajeArreglo)
-    mensajeArreglo.forEach(currentElement => {
-        let INDEX_ORIGINAL = inner_ALFABETO.indexOf(currentElement)
-        arregloFinal.push(inner_ALFABETO_DESPLAZADO[INDEX_ORIGINAL]) 
-         
-     });
-    let mensajeCifrado = arregloFinal.join('')
-    return mensajeCifrado
-}
+  
 
-
-let mensajeACifrar = "holaffffffffff"
-// console.log(ALFABETO_ORIGINAL)
-// console.log(ALFABETO_DESPLAZADO)
-// let resultado = cifrar(mensajeACifrar,ALFABETO_ORIGINAL,ALFABETO_DESPLAZADO)
-// console.log(mensajeACifrar)
-// console.log(resultado)
-// let segundoCifrado = cifrar(resultado,ALFABETO_ORIGINAL,ALFABETO_DESPLAZADO)
-// console.log(segundoCifrado)
